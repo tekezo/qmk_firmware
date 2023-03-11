8163d83572b022b81ac81169b3dda35a8125375b
Takayama Fumihiko <tekezo@pqrs.org>
2023-03-11 16:09:40 +0000

    Use SERIAL_NUMBER environment variable to .iSerialNumber

--- tmk_core/protocol/pico/usb_descriptors.c
+++ tmk_core/protocol/pico/usb_descriptors.c
@@ -184,9 +184,9 @@ uint8_t const* tud_descriptor_configuration_cb(uint8_t index) {
 // array of pointer to string descriptors
 char const* string_desc_arr[] = {
     (const char[]){0x09, 0x04},  // 0: is supported language is English (0x0409)
-    STR(MANUFACTURER),          // 1: Manufacturer
-    STR(PRODUCT),               // 2: Product
-    "123456",                    // 3: Serials, should use chip ID
+    STR(MANUFACTURER),           // 1: Manufacturer
+    STR(PRODUCT),                // 2: Product
+    STR(SERIAL_NUMBER),          // 3: Serials, should use chip ID
 };
 
 static uint16_t _desc_str[32];
