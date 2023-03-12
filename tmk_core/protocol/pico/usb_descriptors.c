81461ecacffc64d077700f1c1408885c572895cf
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2023-03-12 11:06:54 +0900

    Set default serial number to 123456

--- tmk_core/protocol/pico/usb_descriptors.c
+++ tmk_core/protocol/pico/usb_descriptors.c
@@ -29,6 +29,10 @@
 #include "usb_descriptors.h"
 #include "util.h"
 
+#ifndef SERIAL_NUMBER
+#define SERIAL_NUMBER 123456
+#endif
+
 /* A combination of interfaces must have a unique product id, since PC will save
  * device driver after the first plug. Same VID/PID with different interface e.g
  * MSC (first), then CDC (later) will possibly cause system error on PC.
